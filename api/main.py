61bd2ae56ce2c5d23c51f84f5b5a3e7700af19e4
bryanzanr <bryanzanr@gmail.com>
2021-09-03 00:36:01 +0700

    Prepare login through web directly

--- api/main.py
+++ api/main.py
@@ -1,10 +1,13 @@
 # save this as main.py
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_login import login_required, current_user, login_user, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
 # from flask import make_response, jsonify
 # import json
 import jsonpickle
 
 from pathlib import Path
+from api import models
 
 # import os.path
 
@@ -55,9 +58,59 @@ def hello():
 def home_view():
     return "<h1>Welcome to Geeks for Geeks</h1>"
 
-@app.route("/index")
-def index():
-    return render_template('index.html')
+@app.route("/index", methods=['GET', 'POST']) # define login page path
+def index(): # define login page fucntion
+    # return "It Works"
+    if request.method=='GET': # if the request is a GET we return the login page
+        return render_template('index.html')
+    else: # if the request is POST the we check if the user exist and with te right password
+        email = request.form.get('email')
+        password = request.form.get('password')
+        remember = True if request.form.get('remember') else False
+        user = models.User.query.filter_by(email=email).first()
+        # check if the user actually exists
+        # take the user-supplied password, hash it, and compare it to the hashed password in the database
+        if not user:
+            flash('Please sign up before!')
+            return redirect(url_for('auth.signup'))
+        elif not check_password_hash(user.password, password):
+            flash('Please check your login details and try again.')
+            return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
+        # if the above check passes, then we know the user has the right credentials
+        login_user(user, remember=remember)
+        return redirect(url_for('main.profile'))
+
+@app.route('/signup', methods=['GET', 'POST'])# we define the sign up path
+def signup(): # define the sign up function
+    if request.method=='GET': # If the request is GET we return the sign up page and forms
+        return render_template('signup.html')
+    else: # if the request is POST, then we check if the email doesn't already exist and then we save data
+        email = request.form.get('email')
+        name = request.form.get('name')
+        password = request.form.get('password')
+        user = models.User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
+        if user: # if a user is found, we want to redirect back to signup page so user can try again
+            flash('Email address already exists')
+            return redirect(url_for('auth.signup'))
+        # create a new user with the form data. Hash the password so the plaintext version isn't saved.
+        new_user = models.User(email=email, name=name, password=generate_password_hash(password, method='sha256')) #
+        # add the new user to the database
+        print(new_user)
+        # db.session.add(new_user)
+        # db.session.commit()
+        return redirect(url_for('auth.login'))
+    
+
+@app.route("/profile") # profile page that return 'profile'
+@login_required
+def profile():
+    return render_template('profile.html', name=current_user.name)
+
+@app.route('/logout') # define logout path
+@login_required
+def logout(): #define the logout function
+    logout_user()
+    return redirect(url_for('main.index'))
 
 @app.route('/', defaults={'path': ''})
 @app.route("/<string:path>")
