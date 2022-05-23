c02070dd8b2468d0ba9beb43257fc5dfecfaf7a8
izzuddinsyamil <izzuddinsyamil1@gmail.com>
2022-05-23 17:58:22 +0700

    change invalid data source input message

--- gui.py
+++ gui.py
@@ -14,6 +14,7 @@ for f in files:
 
 filename = input('Choose data source (1/2/3/...): ')
 while not filename.isdigit():
+    print('please input valid data source')
     filename = input('Choose data source (1/2/3/...): ')
 
 filename = int(filename)
