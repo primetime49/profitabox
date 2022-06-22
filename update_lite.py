e7504f966341fd26c618991e5c1f11a0970294c9
primetime49 <adityo.anggraito@gmail.com>
2022-06-22 09:59:46 +0200

    limit update lite

--- update_lite.py
+++ update_lite.py
@@ -29,6 +29,7 @@ movie_list = build_list(your_list)
 start_time = time.time()
 year = int(input('From year: '))
 until = int(input('Until year: '))
+max_count = int(input('Max update count: '))
 while year <= until:
     count = 0
     year_time = time.time()
@@ -66,76 +67,77 @@ while year <= until:
                 new_movies.append(movie)
             
             href = movie.href
-            
-            # IMDB API
-            imdb_url = imdb+href.split('/')[2]
-            payload = {}
-            headers= {}
-            clean = json.loads(requests.request("GET", imdb_url, headers=headers, data = payload).text)
-            
-            # BUDGET
-            try:
-                movie.budget = int(BO_number(clean['boxOffice']['budget'].split(' (')[0]))
-            except:
-                print('No budget reported for '+movie.name)
-            
-            # DIRECTOR
-            try:
-                movie.director = clean['directors']
-            except:
-                print('No director found for '+movie.name)
-                
-            # CAST
-            try:
-                cast_list = []
-                for a in clean['actorList']:
-                    cast_list.append(a['name'])
-                movie.cast = ', '.join(cast_list)
-            except Exception as e:
-                print(e)
-                print('Failed to get casts for '+movie.name)
-            
-            # PRODUCTION CO.
-            try:
-                movie.prod = clean['companies']
-            except Exception as e:
-                print(e)
-                print('Failed to get production co. for '+movie.name)
-                
-            # IMDB SCORE
-            try:
-                movie.score = float(clean['imDbRating'])
-            except:
-                print('Failed to get imdb score of '+movie.name)
-            
-            # IMDB REVIEW COUNT
-            try:
-                movie.reviews = int(clean['imDbRatingVotes'])
-            except:
-                print('Failed to get number of reviews for '+movie.name)
-            
-            # MPAA RATING
-            try:
-                movie.rating = clean['contentRating']
-            except:
-                print('No MPAA Rating for '+movie.name)
-                
-            # RUNTIME
-            try:
-                movie.runtime = clean['runtimeMins']
-            except:
-                print('No runtime found for '+movie.name)
-            
-            # GENRES
-            try:
-                movie.genres = clean['genres']
-            except:
-                print('No genres found for '+movie.name)
 
-            movie.runtime = runtiming(movie.runtime)
+            if movie.budget == 0 and count < max_count:    
+                # IMDB API
+                imdb_url = imdb+href.split('/')[2]
+                payload = {}
+                headers= {}
+                clean = json.loads(requests.request("GET", imdb_url, headers=headers, data = payload).text)
+                count += 1
+                
+                # BUDGET
+                try:
+                    movie.budget = int(BO_number(clean['boxOffice']['budget'].split(' (')[0]))
+                except:
+                    print('No budget reported for '+movie.name)
+                
+                # DIRECTOR
+                try:
+                    movie.director = clean['directors']
+                except:
+                    print('No director found for '+movie.name)
+                    
+                # CAST
+                try:
+                    cast_list = []
+                    for a in clean['actorList']:
+                        cast_list.append(a['name'])
+                    movie.cast = ', '.join(cast_list)
+                except Exception as e:
+                    print(e)
+                    print('Failed to get casts for '+movie.name)
+                
+                # PRODUCTION CO.
+                try:
+                    movie.prod = clean['companies']
+                except Exception as e:
+                    print(e)
+                    print('Failed to get production co. for '+movie.name)
+                    
+                # IMDB SCORE
+                try:
+                    movie.score = float(clean['imDbRating'])
+                except:
+                    print('Failed to get imdb score of '+movie.name)
+                
+                # IMDB REVIEW COUNT
+                try:
+                    movie.reviews = int(clean['imDbRatingVotes'])
+                except:
+                    print('Failed to get number of reviews for '+movie.name)
+                
+                # MPAA RATING
+                try:
+                    movie.rating = clean['contentRating']
+                except:
+                    print('No MPAA Rating for '+movie.name)
+                    
+                # RUNTIME
+                try:
+                    movie.runtime = clean['runtimeMins']
+                except:
+                    print('No runtime found for '+movie.name)
+                
+                # GENRES
+                try:
+                    movie.genres = clean['genres']
+                except:
+                    print('No genres found for '+movie.name)
+                    
+                movie.runtime = runtiming(movie.runtime)
 
             print(movie.name+" --- %s seconds ---\n" % (time.time() - movie_time))
-            count += 1
             try:
                 movies_year.remove(movie)
             except:
